4df12aec5d06d1b3ffb129bd5de6917c8b0f34e6
mohammed <mohammedfuta@gmail.com>
2022-07-16 21:57:01 +0000

    now you'll have a csv of states you dont know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import pandas
 import turtle
-from turtle import Screen,Turtle
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.title('U.S States Game')
@@ -8,23 +9,25 @@ screen.addshape(image)
 turtle.shape(image)
 screen.tracer(0)
 
-import pandas
-
 data = pandas.read_csv("50_states.csv")
 
 game_is_on = True
 score = 0
+unknown_states = data.state.to_list()
 while game_is_on:
     user_input = screen.textinput(title=f"{score}/50 States Guessed", prompt="What's another state name?").lower()
+    if user_input == 'exit':
+        break
     if user_input.title() in data.state.to_list():
         score += 1
         state = Turtle()
         state.penup()
         state.hideturtle()
         state_row = data[data.state == user_input.title()]
-        state.goto(int(state_row.x),int(state_row.y))
+        state.goto(int(state_row.x), int(state_row.y))
         state.write(user_input)
+        unknown_states.remove(user_input.title())
     screen.update()
 
-
-screen.exitonclick()
+unknown_states_csv =pandas.DataFrame(unknown_states)
+unknown_states_csv.to_csv('states_to_learn.csv')
